acc3575fa6239d021280fc911e0ca1a70b1ac958
Mark Tomm <marktomm90@gmail.com>
2015-01-06 21:52:08 +0200

    Lots of fundamental changes (init 3)

---
+++ client/js/translations.js
@@ -0,0 +1,11 @@
+if (Meteor.isClient) {
+  i18n.setDefaultLanguage('ee');
+  
+  i18n.map('en', {
+    about_us: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut lectus massa. Suspendisse pulvinar ante eros, quis feugiat enim varius eget. Integer varius est vel ex molestie, et lacinia nulla mattis. Nulla eget leo vitae erat tristique viverra. Nunc blandit purus eu tortor gravida elementum. In dictum luctus mauris at commodo. Suspendisse potenti. Donec diam quam, sollicitudin vitae rutrum eu, suscipit in arcu. Phasellus sit amet feugiat orci. Aenean ultricies turpis vitae lacus pellentesque, eget accumsan ipsum mollis. Maecenas rutrum mattis ligula, non accumsan leo placerat sed. Nam nec metus nisi. Nulla non arcu a felis mollis fringilla ac eget sem. Proin varius odio tortor, quis faucibus nulla lobortis et. Donec arcu nunc, ullamcorper vitae tempus quis, iaculis et nibh. Etiam vel sapien semper, hendrerit nisi id, accumsan risus. Sed odio purus, sagittis ac purus eleifend, dictum blandit ipsum. Cras volutpat eu lorem sit amet rhoncus.'
+  });
+  
+  i18n.map('ee', {
+    about_us: 'Kuradi Raisk!Kuradi Raisk!Kuradi Raisk!Kuradi Raisk!Kuradi Raisk!Kuradi Raisk!Kuradi Raisk!Kuradi Raisk!'
+  });
+}
--- visa.js
+++ visa.js
@@ -1,13 +1,34 @@
+Tasks = new Mongo.Collection("UserForm");
+
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
+  Session.set("currentLang" , i18n.getLanguage());
+  
   Template.header.helpers({
-    img_logo: "/img/sample_logo.png"
+    dev_header: "none",
+    isCurrentLang: function(lang){
+      return Session.equals('currentLang', lang);
+    }
   });
-
-  Template.hello.events({
+  
+  Template.header.events({
+    "click #lang-ee": function(){
+      i18n.setLanguage('ee');
+      Session.set("currentLang" , i18n.getLanguage());
+    },
+    "click #lang-en": function(){
+      i18n.setLanguage('en');
+      Session.set("currentLang" , i18n.getLanguage());
+    }
   });
+  
+  Template.body.events({
+    "submit #fields1": function(event){
+      return false;
+    }
+  });
+  
 }
 
 if (Meteor.isServer) {
